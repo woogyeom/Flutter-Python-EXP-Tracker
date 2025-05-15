5fed52579bcf85f1d9787611fd38210c332e42ea
woogyeom <woogyeom99@gmail.com>
2025-05-15 18:21:49 -0400

    Add expected end time calculation and update display logic for timer

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -46,6 +46,8 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
   Duration updateInterval = const Duration(seconds: 1);
   Duration timerEndTime = Duration.zero;
   Duration _elapsedTime = Duration.zero;
+  DateTime? _expectedEndTime;
+  int _nextHourCount = 1;
 
   // 경험치/레벨 관련 변수
   int initialExp = 0;
@@ -218,7 +220,7 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
         timerEndTime = Duration(seconds: config["timerEndTime"] ?? 0);
         _audioPlayer.setVolume(config["volume"] ?? 0.5);
         showAverage = Duration(seconds: config["showAverage"] ?? 0);
-        showExpectedTime = config["showExpectedTime"] ?? true;
+        showExpectedTime = config["showExpectedTime"] ?? false;
       });
       safeLog("Config loaded: $config");
     } catch (e) {
@@ -437,6 +439,8 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
   Future<void> _startTimer() async {
     _safeSetState(() {
       isRunning = true;
+      _nextHourCount = _elapsedTime.inHours + 1;
+      _expectedEndTime = DateTime.now().add(Duration(hours: _nextHourCount));
     });
 
     // 초기 데이터 fetch 및 UI 업데이트
@@ -643,16 +647,6 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
 
   @override
   Widget build(BuildContext context) {
-    // 스탑워치가 실행 중일 때만 예상 시각 계산
-    final now = DateTime.now();
-    final elapsedHours = _elapsedTime.inHours;
-    final nextHour = elapsedHours + 1;
-    final nextHourTime =
-        now.add(Duration(hours: nextHour, seconds: -_elapsedTime.inSeconds));
-    final expectedEndTimeStr = isRunning
-        ? "${nextHourTime.hour.toString().padLeft(2, '0')}:${nextHourTime.minute.toString().padLeft(2, '0')}:${nextHourTime.second.toString().padLeft(2, '0')}"
-        : "XX:XX:XX";
-
     return CupertinoPageScaffold(
       backgroundColor: isRunning
           ? CupertinoColors.darkBackgroundGray.withAlpha(150)
@@ -825,19 +819,19 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
                       ],
                     ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (showExpectedTime)
+                  if (showExpectedTime)
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
                         Text(
-                          "$nextHour 시간 후 예상 시각: $expectedEndTimeStr",
+                          "$_nextHourCount시간 뒤: ${_expectedEndTime != null ? DateFormat('HH:mm:ss').format(_expectedEndTime!) : "XX:XX:XX"}",
                           style: const TextStyle(
                             color: CupertinoColors.systemGrey6,
                             fontSize: 12,
                           ),
                         ),
-                    ],
-                  ),
+                      ],
+                    ),
                   // 경험치 UI
                   Flexible(
                     child: FittedBox(
@@ -848,7 +842,7 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
                         children: [
                           Text(
                             !isInitValueInserted
-                                ? '? [X.XX%]'
+                                ? 'XX [X.XX%]'
                                 : '${numberFormat.format(totalExp)} [${totalPercentage.toStringAsFixed(2)}%]',
                             style: GoogleFonts.notoSans(
                               textStyle: const TextStyle(
@@ -863,7 +857,7 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
                           if (showAverage != Duration.zero)
                             Text(
                               !isInitValueInserted
-                                  ? '? [?.??%] / ${showAverage.inMinutes}분'
+                                  ? 'XX [X.XX%] / ${showAverage.inMinutes}분'
                                   : '${numberFormat.format(averageExp)} [${averagePercentage.toStringAsFixed(2)}%] / ${showAverage.inMinutes}분',
                               style: GoogleFonts.notoSans(
                                 textStyle: const TextStyle(
@@ -890,7 +884,7 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
                           children: [
                             Text(
                               !isInitValueInserted
-                                  ? '???? 메소'
+                                  ? 'XXXX 메소'
                                   : '${numberFormat.format(totalMeso)} 메소',
                               style: GoogleFonts.notoSans(
                                 textStyle: const TextStyle(
@@ -905,7 +899,7 @@ class _MainScreenState extends State<MainScreen> with WindowListener {
                             if (showMeso && showAverage != Duration.zero)
                               Text(
                                 !isInitValueInserted
-                                    ? '???? 메소 / ${showAverage.inMinutes}분'
+                                    ? 'XXXX 메소 / ${showAverage.inMinutes}분'
                                     : '${numberFormat.format(averageMeso)} 메소 / ${showAverage.inMinutes}분',
                                 style: GoogleFonts.notoSans(
                                   textStyle: const TextStyle(
